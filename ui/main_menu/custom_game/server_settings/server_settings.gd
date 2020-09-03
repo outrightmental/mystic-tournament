20b318ef44cc2a8356bc775488fa791cede8a780
Shatur95 <genaloner@gmail.com>
2020-09-04 00:11:33 +0300

    Remove extra server settings synchronization

--- ui/main_menu/custom_game/server_settings/server_settings.gd
+++ ui/main_menu/custom_game/server_settings/server_settings.gd
@@ -20,31 +20,7 @@ func set_editable(editable: bool):
 		_enable_teams.disabled = false
 		_teams_count.editable = true
 		_players_count.editable = true
-		# warning-ignore:return_value_discarded
-		_teams_count.connect("value_changed", self, "_set_teams_count")
-		# warning-ignore:return_value_discarded
-		_players_count.connect("value_changed", self, "_set_players_count")
-		# warning-ignore:return_value_discarded
-		_enable_teams.connect("toggled", self, "_set_teams_enabled")
 	else:
 		_enable_teams.disabled = true
 		_teams_count.editable = false
 		_players_count.editable = false
-		if _enable_teams.is_connected("toggled", self, "_set_teams_enabled"):
-			_enable_teams.disconnect("toggled", self, "_set_teams_enabled")
-		if _teams_count.is_connected("value_changed", self, "_set_teams_count"):
-			_teams_count.disconnect("value_changed", self, "_set_teams_count")
-		if _players_count.is_connected("value_changed", self, "_set_players_count"):
-			_players_count.disconnect("value_changed", self, "_set_players_count")
-
-
-func _set_teams_enabled(enable: bool):
-	_enable_teams.rset("pressed", enable)
-
-
-func _set_teams_count(count: float):
-	_teams_count.rset("value", count)
-
-
-func _set_players_count(count: float):
-	_players_count.rset("value", count)
