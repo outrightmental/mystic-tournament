f776f1e13153fa9e13f4f5f073d8127833322eb5
Shatur95 <genaloner@gmail.com>
2020-09-06 16:28:00 +0300

    Sync server settings with clients

--- ui/main_menu/custom_game/server_settings/server_settings.gd
+++ ui/main_menu/custom_game/server_settings/server_settings.gd
@@ -15,6 +15,14 @@ func _ready():
 	set_editable(false)
 	_set_teams_enabled(teams_enabled.pressed)
 
+	# Allow to sync server configuration over network
+	teams_enabled.rset_config("pressed", MultiplayerAPI.RPC_MODE_PUPPET)
+	players_count.rset_config("value", MultiplayerAPI.RPC_MODE_PUPPET)
+	teams_count.rset_config("value", MultiplayerAPI.RPC_MODE_PUPPET)
+	players_in_team.rset_config("value", MultiplayerAPI.RPC_MODE_PUPPET)
+	# warning-ignore:return_value_discarded
+	get_tree().connect("network_peer_connected", self, "_send_settings_to_peer")
+
 
 func set_editable(editable: bool):
 	teams_enabled.disabled = !editable
@@ -33,3 +41,12 @@ func _set_teams_enabled(enabled: bool) -> void:
 		players_in_team.value = ceil(players_count.value / teams_count.value)
 	else:
 		players_count.value = players_in_team.value * teams_count.value
+
+
+func _send_settings_to_peer(id: int) -> void:
+	if !get_tree().is_network_server():
+		return
+	teams_enabled.rset_id(id, "pressed", teams_enabled.pressed)
+	players_count.rset_id(id, "value", players_count.value)
+	teams_count.rset_id(id, "value", teams_count.value)
+	players_in_team.rset_id(id, "value", players_in_team.value)
