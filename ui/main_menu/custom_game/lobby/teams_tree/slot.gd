29b7c1b8bd3431fbc08133572f744937ded1b5e1
Shatur95 <genaloner@gmail.com>
2020-09-27 13:25:55 +0300

    Do not display join button for the current slot

--- ui/main_menu/custom_game/lobby/teams_tree/slot.gd
+++ ui/main_menu/custom_game/lobby/teams_tree/slot.gd
@@ -26,13 +26,6 @@ func _init(team, slot_id: int) -> void:
 	_tree_item = team.get_parent().create_item(team.get_tree_item())
 	self.id = slot_id
 
-	# TODO: Replace with icon
-	var temp_image = ImageTexture.new()
-	temp_image.create(15, 15, Image.FORMAT_BPTC_RGBA)
-
-	_tree_item.add_button(JOIN_BUTTON, temp_image, -1, false, "Join")
-
-
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PREDELETE:
 		_tree_item.free()
@@ -45,13 +38,31 @@ puppetsync func set_id(value: int) -> void:
 
 	var previous_id: int = id
 	id = value
-	_update_text()
 
+	if get_tree().get_network_unique_id() == id:
+		_remove_button(JOIN_BUTTON)
+	else:
+		_add_button(JOIN_BUTTON)
+
+	_update_text()
 	emit_signal("id_changed", previous_id, id)
 
 
 func get_tree_item() -> TreeItem:
 	return _tree_item
+
+
+func _add_button(button_type: int) -> void:
+	# TODO: Replace with icon
+	var temp_image = ImageTexture.new()
+	temp_image.create(15, 15, Image.FORMAT_BPTC_RGBA)
+
+	_tree_item.add_button(button_type, temp_image, 0, false, "Join")
+
+
+func _remove_button(button_type: int) -> void:
+	if _tree_item.get_button_count(JOIN_BUTTON) == 1:
+		_tree_item.erase_button(button_type, 0)
 
 
 func _update_text() -> void:
