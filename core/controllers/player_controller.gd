52491d965021e84f7c124e9233faf38fa169f89b
Shatur95 <genaloner@gmail.com>
2020-12-03 21:31:00 +0200

    Specify types

--- core/controllers/player_controller.gd
+++ core/controllers/player_controller.gd
@@ -41,8 +41,8 @@ func _physics_process(delta: float) -> void:
 	if not input_enabled:
 		return
 
-	var x_strength = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var z_strength = Input.get_action_strength("move_back") - Input.get_action_strength("move_front")
+	var x_strength: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var z_strength: float = Input.get_action_strength("move_back") - Input.get_action_strength("move_front")
 
 	var direction := Vector3()
 	direction += _camera.global_transform.basis.x * x_strength
